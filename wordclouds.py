c8a2e394493c6a20c24a73a65dca74779d81ea76
Pranshu Gupta <pranshug258@outlook.com>
2017-05-02 12:32:51 +0530

    generated a wordcloud

--- wordclouds.py
+++ wordclouds.py
@@ -6,4 +6,4 @@ text = open('output/words.txt').read()
 
 # Generate a word cloud image
 wordcloud = WordCloud(width=1200, height=1000).generate(text)
-wordcloud.to_file("output/images/wordcloud.png")
+wordcloud.to_file("images/wordcloud.png")
